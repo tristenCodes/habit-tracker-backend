61cbe0d125d065423bf0c0f511fd9eefd5db47bc
Tristen <79072811+protonvert@users.noreply.github.com>
2024-11-02 12:52:11 -0500

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HabitTrackerBackend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
