9956992d9ef510fe7a8e354b0d9df88296250bff
Tristen <79072811+protonvert@users.noreply.github.com>
2024-11-03 00:07:25 -0500

    Add task model
    First task is now displaying in a non-ideal format

--- Controllers/HabitController.cs
+++ Controllers/HabitController.cs
@@ -17,20 +17,25 @@ namespace HabitTrackerBackend.Controllers
         [HttpGet]
         public IActionResult GetAllHabits()
         {
+            Object[] RowValues = new Object[10];
             var createTable = _connection.CreateCommand();
             createTable.CommandText =
-            @"CREATE TABLE TestTable (ID int)";
-            createTable.ExecuteReader();
+            @"CREATE TABLE IF NOT EXISTS TestTable (ID INTEGER PRIMARY KEY AUTOINCREMENT, HABIT_NAME TEXT, DATE TEXT, COMPLETED INTEGER);
+            INSERT INTO TestTable VALUES(1, 'mow the lawn', datetime(), 0);
+            INSERT INTO TestTable VALUES(2, 'mow the fridge', datetime(), 0);
+            ";
+            createTable.ExecuteNonQuery();
 
             var command = _connection.CreateCommand();
             command.CommandText =
             @"SELECT * FROM TestTable";
-            command.ExecuteReader();
-            var obj = new
+            var reader = command.ExecuteReader();
+            if (reader.HasRows && reader.Read())
             {
-                worked = true,
-            };
-            return Ok(obj);
+                reader.GetValues(RowValues);
+                return Ok(new { result = RowValues });
+            }
+            return Ok(new { worked = false });
         }
     }
 }
---
+++ Models/Task.cs
@@ -0,0 +1,7 @@
+public class Task
+{
+    public int ID { get; set; }
+    public string HabitName { get; set; }
+    public DateTime Date { get; set; }
+    public bool Completed { get; set; }
+}
